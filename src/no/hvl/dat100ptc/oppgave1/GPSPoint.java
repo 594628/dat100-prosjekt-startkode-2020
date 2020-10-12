356ab24c31c9d3107d724934ad11e92d8c1df2fa
Isak <595489@stud.hvl.no>
2020-10-13 00:41:51 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
+++ src/no/hvl/dat100ptc/oppgave1/GPSPoint.java
@@ -2,76 +2,87 @@ package no.hvl.dat100ptc.oppgave1;
 
 import no.hvl.dat100ptc.TODO;
 
-public class GPSPoint {
+public class GPSPoint
+{
 
-	private int time; 
-	private double latitude; 
-	private double longitude; 
-	private double elevation;
-		
-	public GPSPoint(int time, double latitude, double longitude, double elevation) {
+    private int time;
+    private double latitude;
+    private double longitude;
+    private double elevation;
 
-		this.time = time;  
-		this.latitude = latitude; 
-		this.longitude = longitude; 
-		this.elevation = elevation; 
-	}
+    public GPSPoint(int time, double latitude, double longitude, double elevation)
+    {
 
-	// TODO - get/set metoder
-	public int getTime() {
-		
-		
-		return this.time;
-	}
+        this.time = time;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.elevation = elevation;
+    }
 
-	public void setTime(int time) {
-				
-		
-		time = this.time; 
-		
-	}
+    // TODO - get/set metoder
+    public int getTime()
+    {
 
-	public double getLatitude() {
-		
-		
-		return this.latitude; 
-	}
 
-	public void setLatitude(double latitude) {
-		
-		latitude = this.latitude; 
-		
-	}
+        return this.time;
+    }
 
-	public double getLongitude() {
-		
-		return this.longitude; 
-		
-	}
+    public void setTime(int time)
+    {
 
-	public void setLongitude(double longitude) {
-		
-		longitude = this.longitude; 
-		
-	}
 
-	public double getElevation() {
-		
-		return this.elevation; 
-		
-	}
+        time = this.time;
 
-	public void setElevation(double elevation) {
-		
-		elevation = this.elevation; 
-		
-	}
-	
-	public String toString() {
-		
-		String str = (time + " (" + latitude + "," + longitude + ") " + elevation + "\n");
-		
-		return str; 
-	
-	}
+    }
+
+    public double getLatitude()
+    {
+
+
+        return this.latitude;
+    }
+
+    public void setLatitude(double latitude)
+    {
+
+        latitude = this.latitude;
+
+    }
+
+    public double getLongitude()
+    {
+
+        return this.longitude;
+
+    }
+
+    public void setLongitude(double longitude)
+    {
+
+        longitude = this.longitude;
+
+    }
+
+    public double getElevation()
+    {
+
+        return this.elevation;
+
+    }
+
+    public void setElevation(double elevation)
+    {
+
+        elevation = this.elevation;
+
+    }
+
+    public String toString()
+    {
+
+        String str = (time + " (" + latitude + "," + longitude + ") " + elevation + "\n");
+
+        return str;
+
+    }
 }
